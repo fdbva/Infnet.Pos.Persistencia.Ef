6f107fc9f002687bac6cee813b8bcb57126d3c55
Felipe Villela de Andrade <villela.felipe@gmail>
2018-03-10 15:59:18 -0300

    Fez um teste de inserção pelo Console

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entities;
+using Infra;
 
 namespace Console
 {
@@ -10,6 +13,36 @@ namespace Console
     {
         static void Main(string[] args)
         {
+            var db = new DataContext();
+
+            var cursos = db.Cursos
+                .Include(x => x.Professores)
+                .ToList();
+
+            var curso = cursos.FirstOrDefault(x => x.CursoId == 1);
+            var professor = curso.Professores.FirstOrDefault(x => x.ProfessorId == 1);
+
+            var turma = new Turma
+            {
+                DataInicio = DateTime.Now,
+                DataTermino = DateTime.Now.AddDays(15),
+                Curso = curso,
+                Professor = professor,
+                Alunos = new List<Aluno>
+                {
+                    new Aluno
+                    {
+                        Nome = "Joãozinho",
+                        Usuario = new Usuario
+                        {
+                            Email = "joaozinho@ies.edu.br",
+                            Senha = "12345"
+                        }
+                    }
+                }
+            };
+            db.Turmas.Add(turma);
+            db.SaveChanges();
         }
     }
 }
